0d5734b200f43620f736e2c9f8c74401fca261a2
Navrose <navrosesinghjohal@gmail.com>
2024-11-26 23:59:53 -0500

    duplicates removed

--- pandas2.py
+++ pandas2.py
@@ -1,3 +1,11 @@
 import pandas as pd
-sample_excel_data = pd.read_excel("Project-Management-Sample-Data.xlsx",header=5)
-print(sample_excel_data.head())
+sample_excel_data = pd.read_excel("Project-Management-Sample-Data.xlsx")
+sample_excel_data.drop(index=[0,1,2,3,4],inplace=True, columns=["Unnamed: 0"])
+sample_excel_data.reset_index(drop=True)
+sample_excel_data.rename(columns={"index":"Index"},inplace=True)
+sample_excel_data = sample_excel_data.reset_index(drop=True)
+
+# sample_excel_data = sample_excel_data[sample_excel_data.duplicated(['Unnamed: 3'],keep='first')]
+print(sample_excel_data)
+
+
